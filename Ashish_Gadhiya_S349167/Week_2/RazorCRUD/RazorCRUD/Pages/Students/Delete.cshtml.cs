40d137d768cde48c04596b01fe78edb90d76ed66
Ashishgadhiya <90877084+Ashishgadhiya@users.noreply.github.com>
2022-09-07 13:36:22 +0930

    Razor page with Sccafolding

---
+++ Ashish_Gadhiya_S349167/Week_2/RazorCRUD/RazorCRUD/Pages/Students/Delete.cshtml.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using RazorCRUD.Data;
+using RazorCRUD.Model;
+
+namespace RazorCRUD.Pages.Students
+{
+    public class DeleteModel : PageModel
+    {
+        private readonly RazorCRUD.Data.RazorCRUDContext _context;
+
+        public DeleteModel(RazorCRUD.Data.RazorCRUDContext context)
+        {
+            _context = context;
+        }
+
+        [BindProperty]
+      public student student { get; set; } = default!;
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null || _context.student == null)
+            {
+                return NotFound();
+            }
+
+            var student = await _context.student.FirstOrDefaultAsync(m => m.Id == id);
+
+            if (student == null)
+            {
+                return NotFound();
+            }
+            else 
+            {
+                student = student;
+            }
+            return Page();
+        }
+
+        public async Task<IActionResult> OnPostAsync(int? id)
+        {
+            if (id == null || _context.student == null)
+            {
+                return NotFound();
+            }
+            var student = await _context.student.FindAsync(id);
+
+            if (student != null)
+            {
+                student = student;
+                _context.student.Remove(student);
+                await _context.SaveChangesAsync();
+            }
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
