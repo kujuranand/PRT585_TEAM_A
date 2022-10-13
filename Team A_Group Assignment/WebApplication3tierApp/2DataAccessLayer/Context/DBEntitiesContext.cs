1aa2661f3bb7e50f2190b48726e672c6a5bcbfa1
Ashishgadhiya <ashishgadhiya60@gmail.com>
2022-10-13 18:28:09 +0930

    fresh API's

---
+++ Team A_Group Assignment/WebApplication3tierApp/2DataAccessLayer/Context/DBEntitiesContext.cs
@@ -0,0 +1,23 @@
+﻿using _2DataAccessLayer.Context.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2DataAccessLayer.Context
+{   
+
+    public class DBEntitiesContext : DbContext
+    {
+        public DBEntitiesContext(DbContextOptions<DBEntitiesContext> options) : base(options)
+        {
+        }
+        public DbSet<Student> Students { get; set; }
+        public DbSet<Unit> Units { get; set; }
+
+        //public DbSet<Category> Categories { get; set; }
+    }
+
+}
---
+++ Team A_Group Assignment/WebApplication3tierApp/2DataAccessLayer/Migrations/20221013060342_studenn Migration.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace _2DataAccessLayer.Migrations
+{
+    public partial class studennMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Course",
+                table: "Students",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Email",
+                table: "Students",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<long>(
+                name: "Phone",
+                table: "Students",
+                type: "bigint",
+                nullable: false,
+                defaultValue: 0L);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Course",
+                table: "Students");
+
+            migrationBuilder.DropColumn(
+                name: "Email",
+                table: "Students");
+
+            migrationBuilder.DropColumn(
+                name: "Phone",
+                table: "Students");
+        }
+    }
+}
