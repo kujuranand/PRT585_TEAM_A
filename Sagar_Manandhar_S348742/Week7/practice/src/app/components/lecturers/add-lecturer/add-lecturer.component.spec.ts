99744285d71386ad62673756bdeb3c40200f2dc0
Sagarmdr <112367375+Sagarmdr@users.noreply.github.com>
2022-10-16 15:09:34 +0930

    practice angular

---
+++ Sagar_Manandhar_S348742/Week7/practice/src/app/components/lecturers/add-lecturer/add-lecturer.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { AddLecturerComponent } from './add-lecturer.component';
+
+describe('AddLecturerComponent', () => {
+  let component: AddLecturerComponent;
+  let fixture: ComponentFixture<AddLecturerComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ AddLecturerComponent ]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(AddLecturerComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ Sagar_Manandhar_S348742/Week7/practice/src/app/components/lecturers/lecturers-list/lecturers-list.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from '@angular/core';
+import { Lecturer } from 'src/app/models/lecturer.model';
+import { LecturersService } from 'src/app/services/lecturers.service';
+
+@Component({
+  selector: 'app-lecturers-list',
+  templateUrl: './lecturers-list.component.html',
+  styleUrls: ['./lecturers-list.component.css']
+})
+export class LecturersListComponent implements OnInit {
+
+  lecturers: Lecturer[] = [];
+  constructor(private lecturersService: LecturersService) { }
+
+  ngOnInit(): void {
+    this.lecturersService.getAllLecturers()
+    .subscribe({
+      next: (lecturers) => {
+        this.lecturers = lecturers;
+      },
+      error: (response) => {
+        console.log(response);
+      }
+    });
+    
+  }
+
+}
