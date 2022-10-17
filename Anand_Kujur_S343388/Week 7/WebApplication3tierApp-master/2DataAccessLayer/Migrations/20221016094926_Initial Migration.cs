40e1038114fdc14948f3811207c2993b3bdf4053
Anand <anand.kujur@yahoo.com>
2022-10-17 15:02:24 +0930

    First Screen (Employees)

---
+++ Anand_Kujur_S343388/Week 7/WebApplication3tierApp-master/2DataAccessLayer/Migrations/20221016094926_Initial Migration.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace _2DataAccessLayer.Migrations
+{
+    public partial class InitialMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Employees",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Email = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Phone = table.Column<long>(type: "bigint", nullable: false),
+                    Salary = table.Column<long>(type: "bigint", nullable: false),
+                    Department = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Employees", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "People",
+                columns: table => new
+                {
+                    PersonId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    GivenName = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    FamilyName = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    PreferredName = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    DateOfBirth = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    IsDeleted = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_People", x => x.PersonId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Employees");
+
+            migrationBuilder.DropTable(
+                name: "People");
+        }
+    }
+}
---
+++ Anand_Kujur_S343388/Week 7/WebApplication3tierApp-master/WebApplication3tierApp/Controllers/EmployeesController.cs
@@ -0,0 +1,88 @@
+﻿using _2DataAccessLayer.Context;
+using _2DataAccessLayer.Context.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace WebApplication3tierApp.Controllers
+{
+    [ApiController]
+    [Route("/api/employees")]
+    public class EmployeesController : Controller
+    {
+        private readonly DBEntitiesContext _dBEntitiesContext;
+
+        public EmployeesController(DBEntitiesContext dBEntitiesContext)
+        {
+            _dBEntitiesContext = dBEntitiesContext;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAllEmployees() 
+        {
+            var employees = await _dBEntitiesContext.Employees.ToListAsync();
+            return Ok(employees);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> AddEmployee([FromBody] Employee employeeRequest)
+        { 
+            employeeRequest.Id = Guid.NewGuid();
+            await _dBEntitiesContext.Employees.AddAsync(employeeRequest);
+            await _dBEntitiesContext.SaveChangesAsync();
+            return Ok(employeeRequest);
+        }
+
+        [HttpGet]
+        [Route("{id:Guid}")]
+        public async Task<IActionResult> GetEmployee([FromRoute] Guid id)
+        {
+            var employee =
+                await _dBEntitiesContext.Employees.FirstOrDefaultAsync(x => x.Id == id);
+
+            if (employee == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(employee);
+        }
+        [HttpPut]
+        [Route("{id:Guid}")]
+        public async Task<IActionResult> UpdateEmployee([FromRoute] Guid id, Employee updateEmployeeRequest)
+        {
+            var employee = await _dBEntitiesContext.Employees.FindAsync(id);
+
+            if (employee == null)
+            {
+                return NotFound();
+            }
+
+            employee.Name = updateEmployeeRequest.Name;
+            employee.Email = updateEmployeeRequest.Email;
+            employee.Phone = updateEmployeeRequest.Phone;
+            employee.Salary = updateEmployeeRequest.Salary;
+            employee.Department = updateEmployeeRequest.Department;
+
+            await _dBEntitiesContext.SaveChangesAsync();
+
+            return Ok(employee);
+        }
+
+        [HttpDelete]
+        [Route("{id:Guid}")]
+        public async Task<IActionResult> DeleteEmployee([FromRoute] Guid id)
+        {
+            var employee = await _dBEntitiesContext.Employees.FindAsync(id);
+
+            if (employee == null)
+            {
+                return NotFound();
+            }
+
+            _dBEntitiesContext.Employees.Remove(employee);
+            await _dBEntitiesContext.SaveChangesAsync();
+
+            return Ok(employee);
+        }
+    }
+}
